1c544fefc0896d9ea198962dd2c588fbe7dcc027
Para-cpu <s2300889@edu.bc.fi>
2025-11-20 09:54:37 +0200

    Delivery effects & delivery zone advanced

---
+++ Assets/QuickOutline/Scripts/Outline.cs
@@ -0,0 +1,118 @@
+﻿//
+//  QuickOutline – Safe Version (No Mesh Read Access Required)
+//
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Outline : MonoBehaviour
+{
+    public enum Mode
+    {
+        OutlineAll,
+        OutlineVisible,
+        OutlineHidden,
+        OutlineAndSilhouette,
+        SilhouetteOnly
+    }
+
+    [SerializeField] private Mode outlineMode;
+    [SerializeField] private Color outlineColor = Color.white;
+    [SerializeField, Range(0f, 10f)] private float outlineWidth = 2f;
+
+    private Renderer[] renderers;
+    private Material outlineMaskMaterial;
+    private Material outlineFillMaterial;
+
+    private bool needsUpdate;
+
+    void Awake()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+
+        outlineMaskMaterial = Instantiate(Resources.Load<Material>("Materials/OutlineMask"));
+        outlineFillMaterial = Instantiate(Resources.Load<Material>("Materials/OutlineFill"));
+
+        outlineMaskMaterial.name = "OutlineMask (Instance)";
+        outlineFillMaterial.name = "OutlineFill (Instance)";
+
+        // nothing else needed — removed all mesh accessing
+        needsUpdate = true;
+    }
+
+    void OnEnable()
+    {
+        foreach (var r in renderers)
+        {
+            var mats = r.sharedMaterials.ToList();
+            mats.Add(outlineMaskMaterial);
+            mats.Add(outlineFillMaterial);
+            r.materials = mats.ToArray();
+        }
+    }
+
+    void OnDisable()
+    {
+        foreach (var r in renderers)
+        {
+            var mats = r.sharedMaterials.ToList();
+            mats.Remove(outlineMaskMaterial);
+            mats.Remove(outlineFillMaterial);
+            r.materials = mats.ToArray();
+        }
+    }
+
+    void OnDestroy()
+    {
+        Destroy(outlineMaskMaterial);
+        Destroy(outlineFillMaterial);
+    }
+
+    void Update()
+    {
+        if (needsUpdate)
+        {
+            needsUpdate = false;
+            UpdateMaterialProperties();
+        }
+    }
+
+    void UpdateMaterialProperties()
+    {
+        outlineFillMaterial.SetColor("_OutlineColor", outlineColor);
+        outlineFillMaterial.SetFloat("_OutlineWidth", outlineWidth);
+
+        switch (outlineMode)
+        {
+            case Mode.OutlineAll:
+                outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
+                outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
+                break;
+
+            case Mode.OutlineVisible:
+                outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
+                outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.LessEqual);
+                break;
+
+            case Mode.OutlineHidden:
+                outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
+                outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Greater);
+                break;
+
+            case Mode.OutlineAndSilhouette:
+                outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.LessEqual);
+                outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
+                break;
+
+            case Mode.SilhouetteOnly:
+                outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.LessEqual);
+                outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Greater);
+                outlineFillMaterial.SetFloat("_OutlineWidth", 0f);
+                break;
+        }
+    }
+
+    // Removed all Bake(), LoadSmoothNormals(), SmoothNormals(), CombineSubmeshes()
+}
