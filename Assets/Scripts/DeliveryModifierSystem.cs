d5de57b2c54e94c1c19b64a960b176a5c05be50c
Aleksi Hatakka <aleksi.hatakka@edu.bc.fi>
2026-03-30 10:16:51 +0300

    Main Scene feature integration
    + Added delivery modifier to main scene
    + Added mine spawner to main scene

--- Assets/Scripts/DeliveryModifierSystem.cs
+++ Assets/Scripts/DeliveryModifierSystem.cs
@@ -203,7 +203,6 @@ public class DeliveryModifierSystem : MonoBehaviour
     {
         if (modifierNotificationText == null)
         {
-            Debug.LogWarning("ModifierSystem: No notification text assigned!");
             return;
         }
 
